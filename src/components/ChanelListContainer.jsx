57ce2fe9d95fffb7c2724f62d9bdd86edb207854
basofiibnu <>
2022-03-18 20:36:53 +0700

    implement layout container

--- src/components/ChanelListContainer.jsx
+++ src/components/ChanelListContainer.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
+
+const Sidebar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={HospitalIcon} alt="hospital" width="30" />
+      </div>
+    </div>
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Medical Pager</p>
+  </div>
+);
 
 const ChanelListContainer = () => {
-  return <div>ChanelListContainer</div>;
+  return (
+    <Fragment>
+      <Sidebar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="team" />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList {...listProps} type="messaging" />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="messaging" />
+          )}
+        />
+      </div>
+    </Fragment>
+  );
 };
 
 export default ChanelListContainer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as ChanelContainer } from "./ChanelContainer";
 export { default as ChanelListContainer } from "./ChanelListContainer";
+export { default as ChannelSearch } from "./ChannelSearch";
+export { default as TeamChannelList } from "./TeamChannelList";
+export { default as TeamChannelPreview } from "./TeamChannelPreview";
