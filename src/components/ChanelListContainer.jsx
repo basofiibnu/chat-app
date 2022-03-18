a99939f601f9c11f7ce2f2c0d25768ef240cbbc1
basofiibnu <>
2022-03-18 19:58:44 +0700

    add assets

--- src/components/ChanelListContainer.jsx
+++ src/components/ChanelListContainer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 
 const ChanelListContainer = () => {
   return <div>ChanelListContainer</div>;
