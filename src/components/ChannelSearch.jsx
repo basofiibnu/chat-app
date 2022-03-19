0aa44ca8bf73a6b061baef9b167570844adc4af8
basofiibnu <>
2022-03-19 15:25:08 +0700

    added search dropdown

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { getChannel, useChatContext } from "stream-chat-react";
+import { useChatContext } from "stream-chat-react";
+
+import { ResultsDropdown } from "./";
 
 import { SearchIcon } from "../assets";
 
